b881b8c270683e2031fc877642564f1796541956
Ian Cossentino <icossentino@gmail.com>
2020-11-10 09:34:30 -0500

    commit tree b6c72ac69406cb91d0d20ea5f9c84b70beca41ba

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array[0]
+  elsif array.size == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    array[-1] = "and #{array[-1]}"
+    joined_array = array.join(", ")
+    joined_array
+  end
 end
